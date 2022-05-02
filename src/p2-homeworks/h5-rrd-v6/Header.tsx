a4e4aed332a599c123be641880a4553405c45940
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-05-02 18:58:24 +0300

    add homework

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+import {PATH} from "./Pages";
+import s from '../../p1-main/m1-ui/u1-app/App.module.css'
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
+        <div className={s.list__link}>
+            <NavLink className={s.link} to={PATH.PRE_JUNIOR}>PRE_JUNIOR</NavLink>
+            <NavLink className={s.link} to={PATH.JUNIOR}>JUNIOR</NavLink>
+            <NavLink className={s.link} to={PATH.JUNIOR_PLUS}>JUNIOR_PLUS</NavLink>
 
         </div>
     )
--- src/p2-homeworks/h5-rrd-v6/Pages.tsx
+++ src/p2-homeworks/h5-rrd-v6/Pages.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import {Routes, Route, Navigate} from 'react-router-dom';
+import s from '../../../src/p1-main/m1-ui/u1-app/App.module.css'
 import PreJunior from "./pages/PreJunior";
 import Error404 from "./pages/Error404";
+import Junior from "./pages/Junior";
+import JuniorPlus from "./pages/JuniorPlus";
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
+    JUNIOR: '/junior',
+    JUNIOR_PLUS: '/junior-plus',
     // add paths
 }
 
 function Pages() {
     return (
-        <div>
+        <div className={s.wrapper}>
             {/*Routes выбирает первый подходящий роут*/}
           {/* eslint-disable-next-line react/jsx-no-undef */}
             <Routes>
@@ -19,6 +24,8 @@ function Pages() {
                 <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
 
                 <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
+                <Route path={PATH.JUNIOR} element={<Junior/>}/>
+                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
                 // add routes
 
                 {/*он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
