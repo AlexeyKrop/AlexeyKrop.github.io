25a8afe45070f8614acbdb5afc7f7283312bf15f
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-05-19 08:56:16 +0300

    add style for 7 homework

--- src/p2-homeworks/h8/HW8.tsx
+++ src/p2-homeworks/h8/HW8.tsx
@@ -7,44 +7,48 @@ export type UserType = {
 }
 
 const initialPeople = [
-    {_id: 0, name: 'Кот', age: 3},
-    {_id: 1, name: 'Александр', age: 66},
-    {_id: 2, name: 'Коля', age: 16},
-    {_id: 3, name: 'Виктор', age: 44},
-    {_id: 4, name: 'Дмитрий', age: 40},
-    {_id: 5, name: 'Ирина', age: 55},
+  {_id: 0, name: 'Кот', age: 3},
+  {_id: 1, name: 'Александр', age: 66},
+  {_id: 2, name: 'Коля', age: 16},
+  {_id: 3, name: 'Виктор', age: 44},
+  {_id: 4, name: 'Дмитрий', age: 40},
+  {_id: 5, name: 'Ирина', age: 55},
 ]
 
 function HW8() {
-    const [people, setPeople] = useState<Array<UserType>>(initialPeople) // need to fix any
+  const [people, setPeople] = useState<Array<UserType>>(initialPeople) // need to fix any
 
-    // need to fix any
-    const finalPeople = people.map((p: UserType) => (
-        <div key={p._id}>
-          {p.name} {p.age}
-        </div>
-    ))
+  // need to fix any
+  const finalPeople = people.map((p: UserType) => (
+    <li key={p._id}>
+      {p.name} {p.age}
+    </li>
+  ))
 
-    const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
-    const sortDown = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'down'}))
-    const sortEighteen = () => setPeople(homeWorkReducer(initialPeople, {type: 'check', payload: 18}))
+  const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
+  const sortDown = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'down'}))
+  const sortEighteen = () => setPeople(homeWorkReducer(initialPeople, {type: 'check', payload: 18}))
 
-    return (
-        <div>
-            <hr/>
-            homeworks 8
+  return (
+    <div>
+      <hr/>
+      homeworks 8
 
-            {/*should work (должно работать)*/}
-            {finalPeople}
-            <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
-            <div><SuperButton onClick={sortDown}>sort down</SuperButton></div>
-            <div><SuperButton onClick={sortEighteen}>check 18</SuperButton></div>
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativePeople/>*/}
-            <hr/>
-        </div>
-    )
+      {/*should work (должно работать)*/}
+      <ul className='rounded'>
+        {finalPeople}
+      </ul>
+      <div className={'btn-group'}>
+        <div><SuperButton className={'btn'} onClick={sortUp}>sort up</SuperButton></div>
+        <div><SuperButton className={'btn'} onClick={sortDown}>sort down</SuperButton></div>
+        <div><SuperButton className={'btn'} onClick={sortEighteen}>check 18</SuperButton></div>
+      </div>
+      <hr/>
+      {/*для личного творчества, могу проверить*/}
+      {/*<AlternativePeople/>*/}
+      <hr/>
+    </div>
+  )
 }
 
 export default HW8
