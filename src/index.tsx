07c970d11691b510d33d6e83db79a64adb9ecf86
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-05-30 11:34:27 +0300

    fix react-dom/client

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './p1-main/m1-ui/u1-app/App'
 import * as serviceWorker from './serviceWorker'
 import {Provider} from "react-redux";
 import store from "./p2-homeworks/h10/bll/store";
-// @ts-ignore
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
