152f1f7caad988d67fba6b3f1d0d74a12e5c3734
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-06-04 15:22:02 +0300

    add onChangeRange

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -3,37 +3,39 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
 
 function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
+  const [value1, setValue1] = useState(0)
+  const [value2, setValue2] = useState(100)
 
-    return (
-        <div>
-            <hr/>
-            homeworks 11
+  return (
+    <div>
+      <hr/>
+      homeworks 11
 
-            {/*should work (должно работать)*/}
-            <div>
-                <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
-                />
-            </div>
+      {/*should work (должно работать)*/}
+      <div>
+        <span>{value1}</span>
+        <SuperRange onChangeRange={(value) => {
+          setValue1(value)
+        }}
+          // сделать так чтоб value1 изменялось
+        />
+      </div>
 
-            <div>
-                <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
-                <span>{value2}</span>
-            </div>
+      <div>
+        <span>{value1}</span>
+        <SuperDoubleRange
+          // сделать так чтоб value1 и value2 изменялось
+        />
+        <span>{value2}</span>
+      </div>
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperRange/>*/}
-            {/*<AlternativeSuperDoubleRange/>*/}
-            <hr/>
-        </div>
-    )
+      <hr/>
+      {/*для личного творчества, могу проверить*/}
+      {/*<AlternativeSuperRange/>*/}
+      {/*<AlternativeSuperDoubleRange/>*/}
+      <hr/>
+    </div>
+  )
 }
 
 export default HW11
