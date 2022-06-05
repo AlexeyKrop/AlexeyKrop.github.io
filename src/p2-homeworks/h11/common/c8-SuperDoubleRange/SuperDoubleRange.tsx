ebfa78702f22d754b2f0525c15c66aa3621bd1af
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-06-05 14:46:29 +0300

    add style

--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import s from '../../HW11.module.css'
 import SuperRange from "../c7-SuperRange/SuperRange";
 
@@ -18,28 +18,54 @@ const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
 ) => {
   const [minVal, setMinVal] = useState<number>(0);
   const [maxVal, setMaxVal] = useState<number>(100);
+  const range = useRef<HTMLDivElement>(null);
+
   const onChangeMinRange = (e: any) => {
     const value = Math.min(Number(e.currentTarget.value), maxVal - 1)
     setMinVal(value)
     onChangeRange && onChangeRange([minVal, maxVal])
   }
+
   const onChangeMaxRange = (e: any) => {
     const value = Math.max(Number(e.currentTarget.value), minVal + 1)
     setMaxVal(value)
     onChangeRange && onChangeRange([minVal, maxVal])
+
   }
+  const animate = ({ timing, draw, duration }: any) => {
+    let start = performance.now();
+    requestAnimationFrame(function animate(time) {
+      // timeFraction изменяется от 0 до 1
+      let timeFraction = (time - start) / duration;
+      if (timeFraction > 1) {
+        timeFraction = 1;
+      }
+      // вычисление текущего состояния анимации
+      let progress = timing(timeFraction);
+      draw(progress); // отрисовать её
+      if (timeFraction < 1) {
+        requestAnimationFrame(animate);
+      }
+    });
+  };
+  const getPercent =(value: number) => Math.round(((value - minVal) / (maxVal - minVal)) * 100);
+    if (range.current) {
+      range.current.style.left = `${6}%`;
+      range.current.style.width = `${minVal * 1.4}px`;
+    }
+
   // сделать самому, можно подключать библиотеки
   return (
 
     <>
 
       <input className={`${s.thumb} ${s.thumb_left}`} type="range" value={value[0]} onChange={onChangeMinRange}/>
-      <input className={`${s.thumb} ${s.thumb_right}`} type="range" value={value[1]} onChange={onChangeMaxRange}/>
+      <input className={`${s.thumb} ${s.thumb_right}`} type="range" value={value[1]} onChange={onChangeMaxRange} />
 
 
         <div className={s.slider}>
           <div className={s.slider__track}/>
-          <div className={s.slider__range}/>
+          <div ref={range} className={s.slider__range}/>
           <div className={s.slider__left_value}>{minVal}</div>
           <div className={s.slider__right_value}>{maxVal}</div>
         </div>
