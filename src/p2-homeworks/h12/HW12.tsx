48951fde2501780e17966bd2438f32dfb14ffbf4
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-06-13 16:20:32 +0300

    add HW12

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -3,28 +3,24 @@ import s from "./HW12.module.css";
 import SuperSelect from "../h7/common/c5-SuperSelect/SuperSelect";
 import {useDispatch, useSelector} from "react-redux";
 import {changeThemeC} from "./bll/themeReducer";
+import {AppStoreType} from "../h10/bll/store";
 
 const themes = ['dark', 'red', 'some'];
 
 function HW12() {
-    const theme = 'some'; // useSelector
-    // let a = useSelector(state => state.theme)
-    // console.log(a)
+    const theme = useSelector<AppStoreType, string>(state => state.theme.background)
   const dispatch = useDispatch()
     // useDispatch, onChangeCallback
   const onChangeCallback = (value: string) => {
    dispatch(changeThemeC(value))
-
   }
+  console.log(s[theme + '-text'])
     return (
         <div className={s[theme]}>
             <hr/>
             <span className={s[theme + '-text']}>
                 homeworks 12
             </span>
-
-            {/*should work (должно работать)*/}
-            {/*SuperSelect or SuperRadio*/}
             <SuperSelect options={themes} onChangeOption={onChangeCallback}/>
             <hr/>
         </div>
--- src/p2-homeworks/h12/bll/themeReducer.ts
+++ src/p2-homeworks/h12/bll/themeReducer.ts
@@ -1,15 +1,17 @@
 const initState = {
-
+    background: 'dark',
 };
-
-export const themeReducer = (state = initState, action: ChangeThemeAT): any => { // fix any
+type initStateType = {
+    background: string
+}
+export const themeReducer = (state = initState, action: ChangeThemeAT):initStateType  => { // fix any
     switch (action.type) {
-        case "": {
-            return state;
+        case "CHANGE_THEME": {
+            return {...state, background: action.changeBg};
         }
         default: return state;
     }
 };
 
-export const changeThemeC = (value: string): any => ({type: 'CHANGE_THEME', bg: value} as const);
+export const changeThemeC = (value: string): any => ({type: 'CHANGE_THEME', changeBg: value} as const);
 type ChangeThemeAT = ReturnType<typeof changeThemeC>
