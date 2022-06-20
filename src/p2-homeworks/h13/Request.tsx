8824167068fa512f606d3d9fc1e4b38acc08df86
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-06-20 11:25:09 +0300

    add HW13

---
+++ src/p2-homeworks/h13/Request.tsx
@@ -0,0 +1,27 @@
+import React, {ChangeEvent, useState} from 'react';
+import axios from "axios";
+
+const Request = () => {
+  const [value, setValue] = useState<boolean>(false)
+  const [response, setResponse] = useState<string>('')
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.currentTarget.checked)
+  }
+  const onClickRequestHandler = () => {
+    axios.post(`https://neko-cafe-back.herokuapp.com/auth/test`, {success: value})
+      .then(res => setResponse(res.data.errorText))
+      .catch (error =>  {
+      console.log({...error});
+      console.log(error.response ? error.response.data.errorText : error.message);
+       })
+  }
+  return (
+    <div>
+      <button onClick={onClickRequestHandler}>Кнопка</button>
+      <input onChange={onChangeHandler} type="checkbox" className="type"/>
+      <p>{response}</p>
+    </div>
+  );
+};
+
+export default Request;
