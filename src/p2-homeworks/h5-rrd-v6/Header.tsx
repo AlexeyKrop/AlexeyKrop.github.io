dd6eabe3cbc00446ace9080bf5a89ae6b359ca55
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-08-07 16:19:09 +0300

    feat: add active style from NavLink

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import {PATH} from "./Pages";
 import s from '../../p1-main/m1-ui/u1-app/App.module.css'
 
 function Header() {
-    return (
-        <div className={s.list__link}>
-            <NavLink className={s.link} to={PATH.PRE_JUNIOR}>PRE_JUNIOR</NavLink>
-            <NavLink className={s.link} to={PATH.JUNIOR}>JUNIOR</NavLink>
-            <NavLink className={s.link} to={PATH.JUNIOR_PLUS}>JUNIOR_PLUS</NavLink>
+  const setActive = ({isActive}: { isActive: boolean }) => isActive ? `${s.active}` : `${s.link}`
+  return (
+    <div className={s.list__link}>
+      <NavLink className={setActive} to={PATH.PRE_JUNIOR}>PRE_JUNIOR</NavLink>
+      <NavLink className={setActive} to={PATH.JUNIOR}>JUNIOR</NavLink>
+      <NavLink className={setActive} to={PATH.JUNIOR_PLUS}>JUNIOR_PLUS</NavLink>
 
-        </div>
-    )
+    </div>
+  )
 }
 
 export default Header
